529b3ef15f36dc6871e9e97d806e82353a0fbc66
kajbrz <kajbrz@gmail.com>
2019-10-16 15:54:37 +0200

    Initial Commit

---
+++ Drawer.py
@@ -0,0 +1,57 @@
+import sys
+from random import shuffle
+from copy import deepcopy
+
+
+class Drawer:
+
+    def __init__(self, players, options=None):
+        self.__players = players
+        self.__options = options
+        if self.__options is None:
+            self.__options = self.__generate_options()
+
+    def __generate_options(self):
+        return ["Rozumiesz",
+                "E2E",
+                "Ryzyko",
+                "Chcę wam pomóc",
+                "Capacity",
+                "Milestone",
+                "Business value",
+                "Wspólne rozumienie",
+                "Sync",
+                "Byłem w supporcie",
+                "Legacy Break",
+                "Product care",
+                "A ja mam pytanie",
+                "impediment",
+                "MJE",
+                "Top Prio",
+                "Delivery",
+                "Improvement",
+                "Proces",
+                "Way of working",
+                "Value for money",
+                "Robimy Daily",
+                "Mitygacja",
+                "Inwestygacja",
+                "Value for Money",
+                "Definition of Done",
+                "Testy Automatyczne"]
+
+    def draw(self):
+        packetStack = deepcopy(self.__options)
+        shuffle(packetStack)
+        items = {}
+        for player in self.__players:
+            items[player] = packetStack.pop();
+
+        return items
+
+def main(argv):
+    drawer = Drawer(argv)
+    print(drawer.draw())
+
+if __name__ == "__main__":
+    sys.exit(main(["Kaj", "Adi", "Karol", "Sito", "Małcin"]))
---
+++ DrawerTest.py
@@ -0,0 +1,19 @@
+import unittest
+from Drawer import Drawer
+
+class DrawerTestCase(unittest.TestCase):
+
+    def test_doubled_drawing(self):
+        local_options = ["Option1", "Option2"]
+        drawer = Drawer(["Player1", "Player2"], ["Option1", "Option2"])
+        result = drawer.draw()
+        for player, option in result:
+            self.assertTrue(option in local_options)
+
+    def test_simple_drawing(self):
+        drawer = Drawer(["Player1"], ["Option1"])
+        self.assertEqual({"Player1" : "Option1"}, drawer.draw())
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
