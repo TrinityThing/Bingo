70a1f51b9819c5c9d21a98a2fde46b4c29eda583
kajbrz <kajbrz@gmail.com>
2019-10-18 18:29:38 +0200

    Fix #12 Add simple and nice frontend with actual results

--- draw_app/app.py
+++ draw_app/app.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, request
+from flask import Flask, request, render_template
 from drawer.draw import Drawer
 from drawer.points import PointsContainer
 
@@ -13,13 +13,19 @@ def draw():
     return json.dumps(new_draw)
 
 
+@app.route('/')
+@app.route('/draw/')
+def draw_html():
+    return render_template("draw.html", items=json.loads(draw()))
+
+
 @app.route('/draw/rest/addPoints', methods=['POST'])
 def add_points_by_name():
     if request.method == 'POST':
         """ Example usage:
             curl -d '{"player":"Kaju"}' -X POST http://localhost:5000/draw/rest/addPoints
         """
-        #TODO(kaj): Handle correctness of request
+        # TODO(kaj): Handle correctness of request
         points_container = PointsContainer()
         points_container.set_players_and_options(global_drawer.draw())
 
@@ -30,7 +36,7 @@ def add_points_by_name():
 
         return json.dumps({"Actual Points": player_points})
     else:
-        raise Exception('This view is gone. 405')
+        raise Exception('This templates is gone. 405')
 
 
-global_drawer = Drawer(['Kaju', 'Adi', 'Mariaczi', 'Karol', 'Sito'])
+global_drawer = Drawer(['Kaju', 'Adi', 'Mariaczi', 'Karol', 'Śto'])
