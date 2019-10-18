a9a406e0f15caf64731298bf5d1d85638a79b13f
Marcin Puc <marcin.e.puc@gmail.com>
2019-10-18 21:14:11 +0200

    Add config parsing

--- draw_app/app.py
+++ draw_app/app.py
@@ -1,4 +1,8 @@
+import codecs
+import configparser
 import json
+import sys
+import os
 from flask import Flask, redirect, request, render_template, url_for
 from drawer.draw import Drawer
 from draw_app.models.points import PointsContainer
@@ -51,4 +55,25 @@ def add_points_by_name(player_name):
         raise Exception('This template is gone. 405')
 
 
-global_drawer = Drawer(['Kaju', 'Adi', 'Mariaczi', 'Karol', 'Śto'])
+def get_config():
+    parser = configparser.ConfigParser(allow_no_value=True)
+    parser.optionxform = str
+
+    cfg_path = os.getenv("DRAW_APP_CONFIG_FILE", "config.ini")
+    cfg_file = codecs.open(cfg_path, "r", "utf8")
+    parser.read_file(cfg_file)
+
+    required_sections = ["names", "options"]
+    if not all(item in parser.sections() for item in required_sections):
+        sys.exit("Error: Missing [names] or [options] section in config file")
+
+    names = parser.options("names")
+    options = parser.options("options")
+    if len(names) > len(options):
+        sys.exit("Error: More players than available options")
+
+    return names, options
+
+
+player_names, bingo_options = get_config()
+global_drawer = Drawer(player_names, bingo_options)
